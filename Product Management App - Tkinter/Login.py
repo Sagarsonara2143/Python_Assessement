f6b6e263833f1494684c0231e54001194a97bfcf
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-04-18 22:01:57 +0530

    18.04.2023

--- Product Management App - Tkinter/Login.py
+++ Product Management App - Tkinter/Login.py
@@ -11,14 +11,24 @@ root.title("Product Management App")
 root.resizable(width=False,height=False)                            # it will remove Maximise symbol from web page 
 
 
+# Label and place
 l_email=Label(root,text="Email", font=("Sans-serif 15"))
 l_email.place(x = 30, y=50)
 
 l_pwd = Label(root, text="Password",font=("Sans-serif 15"))
 l_pwd.place(x = 30, y = 100)
 
+
+#Entry box 
 e_email = Entry(root, bd=2,width=30, font=("Sans-serif 15"))
 e_email.place(x = 150, y=50)
 
 e_pwd = Entry(root, bd=2,width=30, font=("Sans-serif 15"))
 e_pwd.place(x = 150, y=100)
+
+#Button
+login=Button(root,text="LOGIN",font=("Sans-serif 15"),fg="black",bg="orange", width=10)
+login.place(x=200,y=150)
+
+root.mainloop()
+
