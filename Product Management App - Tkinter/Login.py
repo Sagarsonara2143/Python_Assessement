4451810287c2ec473ffe1b8fe20c7365e2bd88b3
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-04-18 22:47:33 +0530

    18.04.2023

--- Product Management App - Tkinter/Login.py
+++ Product Management App - Tkinter/Login.py
@@ -2,6 +2,7 @@ from tkinter import *
 import tkinter as tk
 import mysql.connector                                          #mysql connector library
 import tkinter.messagebox as msg
+from PIL import ImageTk, Image
 
 
 def register_page():
@@ -14,19 +15,20 @@ root = tk.Tk()                                                                  
 root.geometry("600x600")                                                    # size of web page
 root.title("Product Management App")
 root.resizable(width=False,height=False)                            # it will remove Maximise symbol from web page 
-root.config(bg="white")
+#root.config(bg="white")
+
 
 l_lable=Label(root,text="Product Management Application", font=("Sans-serif ",20),fg="red",bg="yellow").place(x =70, y=20)
 
 # Label and place
-l_email=Label(root,text="Email", font=("Sans-serif 15"), bg="white")
+l_email=Label(root,text="Email", font=("Sans-serif 15"))
 l_email.place(x = 30, y=100)
 
 l_pwd = Label(root, text="Password",font=("Sans-serif 15"))
 l_pwd.place(x = 30, y = 150)
 
 l_text=Label(root, text="Need an account ?",font=("Sans-serif ",12)).place(x=350, y=200)
-l_reg = Label(root, text="Sign up",font=("Sans-serif 14 underline"),fg="blue").place(x=350, y=220)
+l_reg =Button(root, text="Sign up",font=("Sans-serif 14 underline"),fg="blue",command = register_page, bd=0).place(x=350, y=220)
 
 #Entry box 
 e_email = Entry(root, bd=2,width=30, font=("Sans-serif 15"))
@@ -36,7 +38,7 @@ e_pwd = Entry(root, bd=2,width=30, font=("Sans-serif 15"))
 e_pwd.place(x = 150, y=150)
 
 #Button
-login=Button(root,text="LOGIN",font=("Sans-serif 15"),fg="black",bg="orange", width=10, command = register_page)
+login=Button(root,text="LOGIN",font=("Sans-serif 15"),fg="black",bg="orange", width=10)
 login.place(x=200,y=250)
 
 root.mainloop()
