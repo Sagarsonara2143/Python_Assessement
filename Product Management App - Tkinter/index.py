32f751019f15d1f93458ed202ada2615cd4757f5
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-04-19 19:54:44 +0530

    19.04.2023

--- Product Management App - Tkinter/index.py
+++ Product Management App - Tkinter/index.py
@@ -1,1 +1,25 @@
-print("Hellow World")
+from tkinter import *
+import tkinter as tk
+import mysql.connector                                          #mysql connector library
+import tkinter.messagebox as msg
+from PIL import ImageTk, Image
+
+#Connect to database
+def create_con():
+    return mysql.connector.connect(
+                host = "localhost",
+                user = "root",
+                password = "",
+                database = "python"
+        )
+
+
+root = tk.Tk()                                                                             
+
+root.geometry("600x600")                                                        
+root.title("Product Management App")
+root.resizable(width=False,height=False) 
+
+
+l = Label(root,text="Welcome to Index",font=("Sans-serif",15),fg="red")
+l.place(x=120 ,y=300)
