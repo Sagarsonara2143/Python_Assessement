7fe07e64e283c54ee02d4372a8622c57bab3e603
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-04-20 22:48:38 +0530

    20.04.2023

--- Product Management App - Tkinter/products.py
+++ Product Management App - Tkinter/products.py
@@ -22,8 +22,12 @@ root.resizable(width=False,height=False)
 
 l_lable=Label(root,text="Product Management Page", font=("Sans-serif",20,"bold"),fg="red",bg="black", width=36).place(x =0, y=20)
 
+stock=Button(root,text="Stock",font=("Sans-serif 15"),fg="Blue",bg="white", width=10).place(x=150,y=100)
+add=Button(root,text="ADD",font=("Sans-serif 15"),fg="Blue",bg="white", width=10).place(x=350,y=100)
 
-login=Button(root,text="ADD",font=("Sans-serif 15","bold"),fg="Blue",bg="white", width=10).place(x=200,y=250)
+update=Button(root,text="Update",font=("Sans-serif 15"),fg="Blue",bg="white", width=10).place(x=150,y=180)
+delete=Button(root,text="Delete",font=("Sans-serif 15"),fg="Blue",bg="white", width=10).place(x=350,y=180)
+
 
 root.mainloop()
 
